2d1b6d96c5bcdaabf1b33f927ad7f893077d7f26
28brana <28brana@gmail.com>
2023-01-07 17:00:54 +0530

    fix:deployment

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-const BASE_URL="https://fashonx.herokuapp.com/api";
-const TOKEN=localStorage.getItem("persist:root") && JSON.parse(localStorage.getItem("persist:root")).user && JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser && JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
+const BASE_URL = "https://fashonx-backend.onrender.com/api";
+const TOKEN = localStorage.getItem("persist:root") && JSON.parse(localStorage.getItem("persist:root")).user && JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser && JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
 
-export const publicRequest=axios.create({
-    baseURL:BASE_URL,
+export const publicRequest = axios.create({
+    baseURL: BASE_URL,
 });
 
-export const userRequest=axios.create({
-    baseURL:BASE_URL,
-    headers:{token:`Bearer ${TOKEN}`}
+export const userRequest = axios.create({
+    baseURL: BASE_URL,
+    headers: { token: `Bearer ${TOKEN}` }
 })
